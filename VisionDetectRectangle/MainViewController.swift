ae33a8a2d97dbc6c10860989c905c70121e7b0c8
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-01-08 20:01:35 -0500

    Set the camera input

--- VisionDetectRectangle/MainViewController.swift
+++ VisionDetectRectangle/MainViewController.swift
@@ -6,8 +6,29 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class MainViewController: UIViewController {
+    
+    private let captureSession = AVCaptureSession()
+    
+    private func setCameraInput() {
+        guard let device = AVCaptureDevice.DiscoverySession(
+            deviceTypes: [.builtInWideAngleCamera,
+                          .builtInDualCamera,
+                          .builtInTrueDepthCamera],
+            mediaType: .video,
+            position: .back
+        ).devices.first else {
+            fatalError("No back camera device found.")
+        }
+        
+        let cameraInput = try! AVCaptureDeviceInput(device: device)
+        self.captureSession.addInput(cameraInput)
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
