3a537eddf6c5d18772f7fa1afd35744965faaf6e
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-01-08 19:44:32 -0500

    Initial Commit

---
+++ VisionDetectRectangle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisionDetectRectangle
+//
+//  Created by Caroline LaDouce on 1/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
